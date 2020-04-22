fe58859a8590da71c71cab92c74f18fd793977f4
joaovitor <jvarantescabraltrab@gmail.com>
2020-04-21 23:02:21 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var helloWorld = "Hello World!"; //var declara variuáveis
+//let para declarar variáveis num escopo de código
+const pi = 3.14//const para declarar constantes
+//array variável com índice - vetores
+//funções de conversão parseInt parseFloat toString
+console.log(helloWorld); //escreve olá mundo no console,
+alert(helloWorld);       //console.log escreve as informações no console do navegador e alert num popup
