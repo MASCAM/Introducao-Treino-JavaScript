d403e49a075e874a5acf3c84fe4fd4b560178869
joaovitor <jvarantescabraltrab@gmail.com>
2020-04-21 23:42:40 -0300

    mais alterações

--- index.js
+++ index.js
@@ -8,5 +8,30 @@ alert(helloWorld);       //console.log escreve as informações no console do na
 
 let a = 10;
 const b = "10";
-console.log(a == b); //validação ignora o tipo de dado
-console.log(a === b); //3 === valida também o tipo de dado
+console.log(a == b);      //validação ignora o tipo de dado
+console.log(a === b);     //3 === valida também o tipo de dado
+console.log(a != b);      //nega a comparação do valor
+console.log(a !== b);     //nega a comparação do valor e tipo de dado 
+console.log(a == b && typeof b == 'string');   //AND e tipo de variável
+console.log(a == b && typeof a == 'string');   
+console.log(a == b || typeof b == 'string');   //OR e tipo de variável
+console.log(a == b || typeof a == 'string')
+
+let cor = "verde";
+if (cor === "verde") {
+
+    console.log("siga");
+
+} else if (cor === "amarelo" ) {
+
+    console.log("atenção");
+
+} else if (cor == "vermelho") {
+
+    console.log("pare");
+
+} else {
+
+    console.log("erro");
+    
+}
